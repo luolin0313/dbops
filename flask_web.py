29ad37ed4bc0a0046b2c33ef458889af9c45cd84
wuyongshenghub <395424820@qq.com>
2017-08-11 16:02:59 +0800

    侧边栏导航

--- flask_web.py
+++ flask_web.py
@@ -1,14 +1,28 @@
 # encoding:utf-8
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('layout.html')
+    return redirect('/idc')
 
+
+@app.route('/user')
+def user():
+    return render_template('user.html')
+
+
+@app.route('/idc')
+def idc():
+    return render_template('idc.html')
+
+
+@app.route('/pc')
+def pc():
+    return render_template('pc.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=9092, debug=True)
